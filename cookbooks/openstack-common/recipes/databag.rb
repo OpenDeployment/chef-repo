4a4060636abad811b2f7111227161b3abd6b5811
Sam Su <sam.debian@gmail.com>
2013-09-18 04:09:17 -0700

    network

--- cookbooks/openstack-common/recipes/databag.rb
+++ cookbooks/openstack-common/recipes/databag.rb
@@ -18,6 +18,11 @@
 # 
 # import initial data from databag with customized user data.
 #
+
+class ::Chef::Recipe
+  include ::Openstack
+end
+
 defaultbag = "openstack"
 if !Chef::DataBag.list.key?(defaultbag)
     Chef::Application.fatal!("databag '#{defaultbag}' doesn't exist.")
@@ -51,7 +56,6 @@ node.override['openstack']['identity']['admin_token'] = mydata['credential']['id
 node.override['openstack']['identity']['tenants'] = ["#{ mydata['credential']['identity']['tenants']['admin']}", "#{ mydata['credential']['identity']['tenants']['service']}"]
 node.override["openstack"]["identity"]["roles"] = ["#{ mydata['credential']['identity']['roles']['admin']}", "#{ mydata['credential']['identity']['roles']['member']}"]
 
-
 node.override['openstack']['identity']['admin_tenant_name'] = mydata['credential']['identity']['tenants']['admin']
 node.override['openstack']['identity']['admin_user'] = mydata['credential']['identity']['users']['admin']['username']
 node.override['openstack']['identity']['admin_password'] = mydata['credential']['identity']['users']['admin']['password']
@@ -67,6 +71,11 @@ node['openstack']['services'].each_key do |service|
         node.set['openstack']['identity']["#{service}"]['tenant'] = mydata['credential']['identity']['users']["#{service}"]['tenant']
     end
 end
+
+
+# network plugins
+node.override["openstack"]["network"]["plugins"] = ['openvswitch', 'openvswitch-agent'] 
+
 
 
 # ======================== OpenStack Endpoints ================================
@@ -147,8 +156,9 @@ node.override['openstack']['endpoints']['metering-api']['scheme'] = mydata['endp
 # set database attributes
 #node.override['openstack']['db']['server_role'] = "os-ops-database"
 node.override['openstack']['db']['service_type'] = mydata['db']['service_type']
-node.override['openstack']['db']['port'] = mydata['db']["#{node['openstack']['db']['service_type']}"]['port']
 node.override['openstack']['db']['bind_address'] = mydata['db']["#{node['openstack']['db']['service_type']}"]['bind_address']
+node.override['openstack']['db']['port'] = mydata['db']["#{node['openstack']['db']['service_type']}"]['port']
+
 
 # Database used by the OpenStack Compute (Nova) service
 node.override['openstack']['db']['compute']['db_type'] = node['openstack']['db']['service_type']
@@ -200,3 +210,136 @@ node.override['openstack']['mq']['port'] = mydata['mq']["#{node['openstack']['mq
 node.override['openstack']['mq']['user'] = mydata['credential']['mq']["#{node['openstack']['mq']['service_type']}"]['username']
 node.override['openstack']['mq']['password'] = mydata['credential']['mq']["#{node['openstack']['mq']['service_type']}"]['password']
 #node.override['openstack']['mq']['vhost'] = "/"
+
+
+
+
+# ============================= Network service Configuration =====================
+# Gets set in the Network Endpoint when registering with Keystone
+node.override["openstack"]["network"]["service_user"] = node['openstack']['identity']['network']['username']
+node.override["openstack"]["network"]["service_role"] = node['openstack']['identity']['network']['role']
+node.override["openstack"]["network"]["service_name"] = node['openstack']['services']['network']['name']
+# node.override["openstack"]["network"]["service_type"] = "network"
+# node.override["openstack"]["network"]["description"] = "OpenStack Networking service"
+
+# The rabbit user's password is stored in an encrypted databag
+# and accessed with openstack-common cookbook library's
+# user_password routine.  You are expected to create
+# the user, pass, vhost in a wrapper rabbitmq cookbook.
+#node.override["openstack"]["network"]["rabbit_server_chef_role"] = "rabbitmq-server"
+node.override["openstack"]["network"]["rabbit"]["username"] = node['openstack']['mq']['user']
+#node.override["openstack"]["network"]["rabbit"]["vhost"] = "/"
+node.override["openstack"]["network"]["rabbit"]["port"] = node['openstack']['mq']['port']
+node.override["openstack"]["network"]["rabbit"]["host"] = node['openstack']['mq']['bind_address']
+#node.override["openstack"]["network"]["rabbit"]["ha"] = false
+
+# The database username for the quantum database
+node.override["openstack"]["network"]["db"]["username"] = mydata['credential']['mysql']['network']['username']
+
+# Used in the Keystone authtoken middleware configuration
+node.override["openstack"]["network"]["service_tenant_name"] = node['openstack']['identity']['network']['tenant']
+node.override["openstack"]["network"]["service_user"] = node['openstack']['identity']['network']['username']
+node.override["openstack"]["network"]["service_role"] = node['openstack']['identity']['network']['role']
+
+node.override["openstack"]["network"]["api"]["bind_interface"] = node['openstack']['endpoints']['network-api']['host']
+
+
+# ============================= L3 Agent Configuration =====================
+
+# Indicates that this L3 agent should also handle routers that do not have
+# an external network gateway configured.  This option should be True only
+# for a single agent in a Quantum deployment, and may be False for all agents
+# if all routers must have an external network gateway
+#node.override["openstack"]["network"]["l3"]["handle_internal_only_routers"] = "False"
+
+# Name of bridge used for external network traffic. This should be set to
+# empty value for the linux bridge
+# node.override["openstack"]["network"]["l3"]["external_network_bridge"] = "br-ex"
+
+# Interface to use for external bridge.
+node.override["openstack"]["network"]["l3"]["external_network_bridge_interface"] = mydata['networking']['public']['interface']
+
+
+# ============================= Metadata Agent Configuration ===============
+
+# The location of the Nova Metadata API service to proxy to (nil uses node.override)
+node.override["openstack"]["network"]["metadata"]["nova_metadata_ip"] = mydata['endpoints']['compute']['service']['host']
+#node.override["openstack"]["network"]["metadata"]["nova_metadata_port"] = 8775
+
+# The name of the secret databag containing the metadata secret
+node.override["openstack"]["network"]["metadata"]["secret_name"] = mydata['credential']['metadata']['password']
+
+
+# ============================= OVS Plugin Configuration ===================
+
+# Type of network to allocate for tenant networks. The node.override value 'local' is
+# useful only for single-box testing and provides no connectivity between hosts.
+# You MUST either change this to 'vlan' and configure network_vlan_ranges below
+# or change this to 'gre' and configure tunnel_id_ranges below in order for tenant
+# networks to provide connectivity between hosts. Set to 'none' to disable creation
+# of tenant networks.
+
+tenant_network_type = mydata['networking']['plugins']['ovs']['tenant_network_type']
+node.override["openstack"]["network"]["openvswitch"]["tenant_network_type"] =tenant_network_type
+
+# Comma-separated list of <physical_network>[:<vlan_min>:<vlan_max>] tuples enumerating
+# ranges of VLAN IDs on named physical networks that are available for allocation.
+# All physical networks listed are available for flat and VLAN provider network
+# creation. Specified ranges of VLAN IDs are available for tenant network
+# allocation if tenant_network_type is 'vlan'. If empty, only gre and local
+# networks may be created
+#
+# Example: network_vlan_ranges = physnet1:1000:2999
+node.override["openstack"]["network"]["openvswitch"]["network_vlan_ranges"] =mydata['networking']['plugins']['ovs']["#{tenant_network_type}"]['network_vlan_ranges']
+
+# Set to True in the server and the agents to enable support
+# for GRE networks. Requires kernel support for OVS patch ports and
+# GRE tunneling.
+node.override["openstack"]["network"]["openvswitch"]["enable_tunneling"] = mydata['networking']['plugins']['ovs']["#{tenant_network_type}"]['enable_tunneling']
+
+# Comma-separated list of <tun_min>:<tun_max> tuples
+# enumerating ranges of GRE tunnel IDs that are available for tenant
+# network allocation if tenant_network_type is 'gre'.
+#
+# Example: tunnel_id_ranges = 1:1000
+node.override["openstack"]["network"]["openvswitch"]["tunnel_id_ranges"] = mydata['networking']['plugins']['ovs']["#{tenant_network_type}"]['tunnel_id_ranges']
+
+# Do not change this parameter unless you have a good reason to.
+# This is the name of the OVS integration bridge. There is one per hypervisor.
+# The integration bridge acts as a virtual "patch bay". All VM VIFs are
+# attached to this bridge and then "patched" according to their network
+# connectivity
+node.override["openstack"]["network"]["openvswitch"]["integration_bridge"] = mydata['networking']['plugins']['ovs']['integration_bridge']
+
+# Only used for the agent if tunnel_id_ranges (above) is not empty for
+# the server.  In most cases, the node.override value should be fine
+node.override["openstack"]["network"]["openvswitch"]["tunnel_bridge"] = mydata['networking']['plugins']['ovs']["#{tenant_network_type}"]['tunnel_bridge']
+
+# Uncomment this line for the agent if tunnel_id_ranges (above) is not
+# empty for the server. Set local_ip to be the local IP address of
+# this hypervisor or set the local_ip_interface parameter to use the IP
+# address of the specified interface.  If local_ip_interface is set
+# it will take precedence.
+local_ip_interface = mydata['networking']['plugins']['ovs']["#{tenant_network_type}"]['local_ip_interface']
+if "#{local_ip_interface}" != ("nil")
+    local_ip= address_for(local_ip_interface)
+     puts "******local_ip=#{local_ip}***"
+else
+    local_ip="nil"
+end
+
+node.override["openstack"]["network"]["openvswitch"]["local_ip"] = local_ip
+node.override["openstack"]["network"]["openvswitch"]["local_ip_interface"] = local_ip_interface
+
+# Comma-separated list of <physical_network>:<bridge> tuples
+# mapping physical network names to the agent's node-specific OVS
+# bridge names to be used for flat and VLAN networks. The length of
+# bridge names should be no more than 11. Each bridge must
+# exist, and should have a physical network interface configured as a
+# port. All physical networks listed in network_vlan_ranges on the
+# server should have mappings to appropriate bridges on each agent.
+#
+# Example: bridge_mappings = physnet1:br-eth1
+node.override["openstack"]["network"]["openvswitch"]["bridge_mappings"] = mydata['networking']['plugins']['ovs']["#{tenant_network_type}"]['bridge_mappings']
+
+
