9cf3e362d2256f69672442b213172074fb3a2b72
Sam Su <sam.debian@gmail.com>
2013-09-21 19:19:18 -0700

    nova

--- cookbooks/openstack-network/attributes/default.rb
+++ cookbooks/openstack-network/attributes/default.rb
@@ -716,7 +716,7 @@ when "fedora", "redhat", "centos" # :pragma-foodcritic: ~FC024 - won't fix this
     "quantum_dhcp_packages" => [ "openstack-quantum" ],
     "quantum_dhcp_build_packages" => [],
     "quantum_l3_packages" => [ "quantum-l3-agent" ],
-    "quantum_openvswitch_packages" => ["openvswitch"],
+    "quantum_openvswitch_packages" => ["openvswitch", "bridge-utils"],
     "quantum_openvswitch_agent_packages" => ["openstack-quantum-openvswitch"],
     "quantum_linuxbridge_agent_packages" => ["openstack-quantum-linuxbridge"],
     "quantum_metadata_agent_packages" => [],
